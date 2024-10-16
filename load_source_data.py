fdc2727a60b0f998a89656bd04ffeb2cc32980c7
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-10-16 20:36:08 +0300

    correctly loading source data into source schema using indexing

--- load_source_data.py
+++ load_source_data.py
@@ -1,9 +1,7 @@
 import pandas as pd
-from sqlalchemy import create_engine
-import geopandas as gpd
-import dotenv 
-from glob import glob
-import os 
+import dotenv, os, json 
+from sqlalchemy import create_engine, text
+from tqdm import tqdm
 
 dotenv.load_dotenv('./secrets.env')
 
@@ -11,54 +9,107 @@ user = os.getenv('POSTGRES_USER')
 password = os.getenv('POSTGRES_PASSWORD')
 database = os.getenv('POSTGRES_DB')
 
-host = 'localhost'  
-port = '5432' 
-
-raw_files = './raw'
-csv_files_list = glob(f"{raw_files}/*.csv")
-geojson_files_list = glob(f"{raw_files}/*.geojson")
+host = 'localhost'
+port = '5432'
 
 engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}')
 
-for file_path in csv_files_list:    
-    filename = file_path.split('/')[-1]
-    table_name = filename.replace(".csv", "")
-    print(f"Seeding data from file {file_path} into {table_name}:")
-    
-    i = 1
-    for chunk in pd.read_csv(file_path, chunksize=5000):
-        if table_name == 'calendar':
-            chunk["adjusted_price"] = chunk["adjusted_price"].apply(lambda x: str(x).replace(',', '').replace('$', '') if pd.notnull(x) else x)
-            chunk["price"] = chunk["price"].apply(lambda x: str(x).replace(',', '').replace('$', '') if pd.notnull(x) else x)
-        
-        print(f"[{filename} -> {table_name}] casting chunk: {i}          Done!")
-        
-        try:
-            print(f"[{filename} -> {table_name}] seeding chunk: {i}")
-            chunk.to_sql(table_name, engine, if_exists='append', index=False)
-            print(f"[{filename} -> {table_name}] seeding chunk: {i}          Done!")
-        except Exception as e:
-            print(f"Error while seeding chunk {i} from file {filename}: {e}")
-        
-        i += 1
+def read_config(path:str)->dict:
+    with open(path, 'r') as file:
+        return json.load(file)
 
+def proecss_calendar(chunk:pd.DataFrame)->pd.DataFrame:
+    chunk["adjusted_price"] = chunk["adjusted_price"].apply(lambda x: str(x).replace(',', '').replace('$', '') if pd.notnull(x) else x)
+    chunk["price"] = chunk["price"].apply(lambda x: str(x).replace(',', '').replace('$', '') if pd.notnull(x) else x)
+    return chunk
 
-for path in geojson_files_list:
-    filename = path.split('/')[-1]
-    table_name = filename.replace(".geojson", "")
-    
-    try:
-        print(f"[{path} --> {table_name}] Reading")
-        gdf = gpd.read_file(path)
+def prepare_public_schema(engine):
+    stmts = [
+        'DROP SCHEMA IF EXISTS public CASCADE;',
+        'CREATE SCHEMA IF NOT EXISTS public;',
+        'CREATE EXTENSION IF NOT EXISTS postgis;'
+    ]
+    with engine.begin() as conn:
+        for stmt in stmts:
+            print(f"Executing: {stmt}")
+            conn.execute(text(stmt))
         
-        print(f"[{path} --> {table_name}] to crs")
+    
+config = read_config("./load_source_config.json")
+
+prepare_public_schema(engine)
+
+for file_path in config:
+    print("*"*100)
+    pair = config.get(file_path)
+    table_name = pair.get('table_name')
+    primary_unique_key = pair.get('primary_unique_key')
+    
+    add_primary_key = False
+    if file_path.endswith("csv"):
+        df = pd.read_csv(file_path, nrows=1)
+        
+        if primary_unique_key not in df.columns:
+            add_primary_key = True
+            
+        print(f"Loading data from {file_path} to {table_name} table ..")
+        
+        show = False
+        for chunk in tqdm(pd.read_csv(
+            file_path,
+            chunksize=15000
+        )):            
+            
+            if not show:
+                print("df before processing")
+                print(chunk.head(10))
+            if add_primary_key:
+                chunk = chunk.reset_index().rename(columns={'index': primary_unique_key})
+            
+            if table_name == 'calendar':
+                chunk = proecss_calendar(chunk)
+            
+            if not show:
+                print("df after processing")
+                print(chunk.head(10))
+            show = True
+            
+            chunk.to_sql(
+                table_name,
+                engine,
+                if_exists='append',
+                index=False)
+            
+            
+    elif file_path.endswith("geojson"):
+        import geopandas as gpd
+        
+        print(f"reading file {file_path}")
+        
+        gdf = gpd.read_file(file_path)
+        print(gdf.head)
+        
+        print(f"Loading file {file_path} into {table_name} table")
+        
+        if primary_unique_key not in gdf.columns:
+            add_primary_key = True
+        
+        if add_primary_key:
+            gdf = gdf.reset_index().rename(columns={'index': primary_unique_key})
+            
         gdf = gdf.to_crs(epsg=4326)
+        gdf.to_postgis(
+            table_name,
+            engine,
+            if_exists='replace',
+        )
         
-        print(f"[{path} --> {table_name}] loading")
-        gdf.to_postgis(table_name, engine, if_exists='replace')
+        print(f"Loading file {file_path} into {table_name} table            Done!")
+    
+    primary_key_alter = text(f"ALTER TABLE {table_name} ADD PRIMARY KEY ({primary_unique_key})")
+    with engine.begin() as conn:
+        print("Creating the primary key constraint!")
+        print(primary_key_alter)
         
-        
-        print(f"[{path} --> {table_name}] Done")
-    except Exception as e:
-            print(f"Error while seeding {path} into {table_name}: {e}")
-        
+        conn.execute(primary_key_alter)
+        print(f"Successfully created the primary key {primary_key_alter} for the table {table_name}")
